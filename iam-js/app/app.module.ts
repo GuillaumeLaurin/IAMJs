d51629f908185a7fb83e86e395a9bb649ffb4a24
Guillaume Laurin <glaurin97@gmail.com>
2026-03-14 09:05:21 -0400

    Add User module and linked it to the App module

--- iam-js/app/app.module.ts
+++ iam-js/app/app.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from '@user/user.module';
 
-@Module({})
+@Module({
+    imports: [
+        UserModule
+    ]
+})
 export class AppModule {}
