1ae3817c79cc01e5f315fd0afdc56044734cc171
GuillaumeIsCoding <115158922+GuillaumeIsCoding@users.noreply.github.com>
2026-03-20 09:46:06 -0400

    Implement Role Guard

---
+++ iam-js/app/modules/user/guards/role/role.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { User } from '@user/entities/user.entity';
+
+@Injectable()
+export class RoleGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<string[]>('roles', [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (!requiredRoles) return true;
+
+    const { user } = context.switchToHttp().getRequest();
+    return (user as unknown as User)?.roles?.some((role) =>
+      requiredRoles.includes(role.name),
+    );
+  }
+}
