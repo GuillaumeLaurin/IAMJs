c055562e205a118c47db08bb8793c6e8b64c0458
Guillaume Laurin <glaurin97@gmail.com>
2026-03-27 19:31:18 -0400

    Fix lint dependencies error

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -2,6 +2,11 @@
 import eslint from '@eslint/js';
 import { configs as airbnbConfigs } from 'eslint-config-airbnb-extended';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import importX from 'eslint-plugin-import-x';
+import stylistic from '@stylistic/eslint-plugin';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
@@ -28,6 +33,13 @@ export default tseslint.config(
 
   // Language & parser options
   {
+    plugins: {
+      'import-x': importX,
+      '@stylistic': stylistic,
+      'react': react,
+      'react-hooks': reactHooks,
+      'jsx-a11y': jsxA11y,
+    },
     languageOptions: {
       globals: {
         ...globals.browser,
